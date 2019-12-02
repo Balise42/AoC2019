c1534a637dad92dc46573449a218a2ec7b058d17
Isabelle Hurbain-Palatin <isabelle@palatin.fr>
2019-12-02 07:47:39 +0100

    day 2 - pre-clean up

---
+++ src/test/kotlin/fr/pasithee/aoc2019/IntcodeTest.kt
@@ -0,0 +1,30 @@
+package fr.pasithee.aoc2019
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class IntcodeTest {
+    @Test
+    fun runShouldWork() {
+        val inputs = arrayListOf(
+            arrayListOf(1, 0, 0, 0, 99),
+            arrayListOf(2, 3, 0, 3, 99),
+            arrayListOf(2, 4, 4, 5, 99, 9),
+            arrayListOf(1, 1, 1, 4, 99, 5, 6, 0, 99),
+            arrayListOf(1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50)
+        )
+        var results = arrayListOf(
+            arrayListOf(2, 0, 0, 0, 99),
+            arrayListOf(2, 3, 0, 6, 99),
+            arrayListOf(2, 4, 4, 5, 99, 9801),
+            arrayListOf(30, 1, 1, 4, 2, 5, 6, 0, 99),
+            arrayListOf(3500, 9, 10, 70, 2, 3, 11, 0, 99, 30, 40, 50)
+        )
+
+        for (i in 0..4) {
+            val program = Intcode(inputs[i])
+            program.run()
+            assertEquals(results[i], program.state())
+        }
+    }
+}
