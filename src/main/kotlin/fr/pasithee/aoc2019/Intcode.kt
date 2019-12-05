6f313153adc9a8a91d2e75dce92021d1a2c05b17
Isabelle Hurbain-Palatin <isabelle@palatin.fr>
2019-12-05 07:05:44 +0100

    day5, bit of clean up

--- src/main/kotlin/fr/pasithee/aoc2019/Intcode.kt
+++ src/main/kotlin/fr/pasithee/aoc2019/Intcode.kt
@@ -1,8 +1,12 @@
 package fr.pasithee.aoc2019
 
-class Intcode(program: List<Int>, noun: Int, verb: Int) {
+import java.lang.UnsupportedOperationException
+
+
+class Intcode(program: List<Int>, noun: Int, verb: Int, var input: Int = 1) {
 
     private val program: MutableList<Int> = program.toMutableList()
+    var output : Int? = null
 
     init {
         this.program[1] = noun
@@ -14,21 +18,81 @@ class Intcode(program: List<Int>, noun: Int, verb: Int) {
     fun run() {
         var instPtr = 0
         while(true) {
-            when(program[instPtr]) {
+            val modes = program[instPtr] / 100
+            when(program[instPtr] % 100) {
                 99 -> return
-                1 -> instPtr += addReferences(program[instPtr + 1], program[instPtr + 2], program[instPtr + 3])
-                2 -> instPtr += mulReferences(program[instPtr + 1], program[instPtr + 2], program[instPtr + 3])
+                1 -> instPtr += add(program[instPtr + 1], program[instPtr + 2], program[instPtr + 3], modes)
+                2 -> instPtr += mul(program[instPtr + 1], program[instPtr + 2], program[instPtr + 3], modes)
+                3 -> instPtr += save(program[instPtr + 1])
+                4 -> instPtr += out(program[instPtr + 1], modes)
+                5 -> instPtr = jit(instPtr, program[instPtr+1], program[instPtr + 2], modes)
+                6 -> instPtr = jif(instPtr, program[instPtr+1], program[instPtr + 2], modes)
+                7 -> instPtr += lt(program[instPtr + 1], program[instPtr + 2], program[instPtr + 3], modes)
+                8 -> instPtr += eq(program[instPtr + 1], program[instPtr + 2], program[instPtr + 3], modes)
+                else -> throw(UnsupportedOperationException("instPtr: " + program[instPtr]))
             }
         }
     }
 
-    private fun addReferences(op1: Int, op2: Int, res: Int) : Int {
-        program[res] = program[op1] + program[op2]
+    private fun jit(instrPtr: Int, op1: Int, op2: Int, modes: Int): Int {
+        val cond = if (modes % 10 == 0) program[op1] else op1
+        val jumpVal = if (modes / 10 == 0) program[op2] else op2
+        if (cond != 0) {
+            return jumpVal
+        } else {
+            return instrPtr + 3
+        }
+    }
+
+    private fun jif(instrPtr: Int, op1: Int, op2: Int, modes: Int): Int {
+        val cond = if (modes % 10 == 0) program[op1] else op1
+        val jumpVal = if (modes / 10 == 0) program[op2] else op2
+        if (cond == 0) {
+            return jumpVal
+        } else {
+            return instrPtr + 3
+        }
+    }
+
+    private fun lt(op1: Int, op2: Int, res: Int, modes: Int) : Int {
+        val v1 = if (modes % 10 == 0) program[op1] else op1
+        val v2 = if (modes / 10 == 0) program[op2] else op2
+        program[res] = if (v1 < v2) 1 else 0
         return 4
     }
 
-    private fun mulReferences(op1: Int, op2: Int, res: Int) : Int {
-        program[res] = program[op1] * program[op2]
+    private fun eq(op1: Int, op2: Int, res: Int, modes: Int) : Int {
+        val v1 = if (modes % 10 == 0) program[op1] else op1
+        val v2 = if (modes / 10 == 0) program[op2] else op2
+        program[res] = if (v1 == v2) 1 else 0
+        return 4
+    }
+
+    private fun out(op: Int, modes: Int): Byte {
+        if (modes == 0) {
+            output = program[op]
+        } else {
+            output = op
+        }
+        return 2
+    }
+
+    private fun save(op: Int): Int {
+        program[op] = input
+        return 2
+    }
+
+    private fun add(op1: Int, op2: Int, res: Int, modes: Int) : Int {
+        val v1 = if (modes % 10 == 0) program[op1] else op1
+        val v2 = if (modes / 10 == 0) program[op2] else op2
+        program[res] = v1 + v2
+        return 4
+    }
+
+    private fun mul(op1: Int, op2: Int, res: Int, modes: Int) : Int {
+        val v1 = if (modes % 10 == 0) program[op1] else op1
+        val v2 = if (modes / 10 == 0) program[op2] else op2
+        program[res] = v1 * v2
         return 4
     }
 
--- src/test/kotlin/fr/pasithee/aoc2019/IntcodeTest.kt
+++ src/test/kotlin/fr/pasithee/aoc2019/IntcodeTest.kt
@@ -11,14 +11,16 @@ class IntcodeTest {
             arrayListOf(2, 3, 0, 3, 99),
             arrayListOf(2, 4, 4, 5, 99, 9),
             arrayListOf(1, 1, 1, 4, 99, 5, 6, 0, 99),
-            arrayListOf(1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50)
+            arrayListOf(1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50),
+            arrayListOf(1002,4,3,4,33)
         )
-        var results = arrayListOf(
+        val results = arrayListOf(
             arrayListOf(2, 0, 0, 0, 99),
             arrayListOf(2, 3, 0, 6, 99),
             arrayListOf(2, 4, 4, 5, 99, 9801),
             arrayListOf(30, 1, 1, 4, 2, 5, 6, 0, 99),
-            arrayListOf(3500, 9, 10, 70, 2, 3, 11, 0, 99, 30, 40, 50)
+            arrayListOf(3500, 9, 10, 70, 2, 3, 11, 0, 99, 30, 40, 50),
+            arrayListOf(1002, 4, 3, 4, 99)
         )
 
         for (i in 0..4) {
@@ -26,5 +28,96 @@ class IntcodeTest {
             program.run()
             assertEquals(results[i], program.state())
         }
+    }
+
+    @Test
+    fun ioShouldWork() {
+        val program = Intcode(arrayListOf(3,0,4,0,99), 0, 4, 78)
+        program.run()
+        assertEquals(78, program.input)
+        assertEquals(78, program.output)
+    }
+
+    @Test
+    fun equalPosShouldReturnFalse() {
+        val program = Intcode(arrayListOf(3,9,8,9,10,9,4,9,99,-1,8), 9, 8, 78)
+        program.run()
+        assertEquals(0, program.output)
+    }
+
+    @Test
+    fun equalPosShouldReturnTrue() {
+        val program = Intcode(arrayListOf(3,9,8,9,10,9,4,9,99,-1,8), 9, 8, 8)
+        program.run()
+        assertEquals(1, program.output)
+    }
+
+    @Test
+    fun equalImmShouldReturnFalse() {
+        val program = Intcode(arrayListOf(3,3,1108,-1,8,3,4,3,99), 3, 1108, 78)
+        program.run()
+        assertEquals(0, program.output)
+    }
+
+    @Test
+    fun equalImmShouldReturnTrue() {
+        val program = Intcode(arrayListOf(3,3,1108,-1,8,3,4,3,99), 3, 1108, 8)
+        program.run()
+        assertEquals(1, program.output)
+    }
+
+    @Test
+    fun ltPosShouldReturnFalse() {
+        val program = Intcode(arrayListOf(3,9,7,9,10,9,4,9,99,-1,8), 9, 7, 78)
+        program.run()
+        assertEquals(0, program.output)
+    }
+
+    @Test
+    fun ltPosShouldReturnTrue() {
+        val program = Intcode(arrayListOf(3,9,7,9,10,9,4,9,99,-1,8), 9, 7, 7)
+        program.run()
+        assertEquals(1, program.output)
+    }
+
+    @Test
+    fun ltImmShouldReturnFalse() {
+        val program = Intcode(arrayListOf(3,9,7,9,10,9,4,9,99,-1,8), 3, 1107, 78)
+        program.run()
+        assertEquals(0, program.output)
+    }
+
+    @Test
+    fun ltImmShouldReturnTrue() {
+        val program = Intcode(arrayListOf(3,3,1107,-1,8,3,4,3,99), 3, 1107, 7)
+        program.run()
+        assertEquals(1, program.output)
+    }
+
+    @Test
+    fun largerExampleShouldWorkLessThan8() {
+        val program = Intcode(arrayListOf(3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,
+            1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,
+            999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99), 21, 1008, 4)
+        program.run()
+        assertEquals(999, program.output)
+    }
+
+    @Test
+    fun largerExampleShouldWorkEquals8() {
+        val program = Intcode(arrayListOf(3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,
+            1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,
+            999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99), 21, 1008, 8)
+        program.run()
+        assertEquals(1000, program.output)
+    }
+
+    @Test
+    fun largerExampleShouldWorkLargerThan8() {
+        val program = Intcode(arrayListOf(3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,
+            1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,
+            999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99), 21, 1008, 45)
+        program.run()
+        assertEquals(1001, program.output)
     }
 }
