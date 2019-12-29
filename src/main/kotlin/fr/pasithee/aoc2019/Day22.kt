d2fe9e8544df5ec960b67b0778417337ca3b2a42
Isabelle Hurbain-Palatin <isabelle@palatin.fr>
2019-12-29 19:09:13 +0100

    day 22 part 2 - URGH.

--- src/main/kotlin/fr/pasithee/aoc2019/Day22.kt
+++ src/main/kotlin/fr/pasithee/aoc2019/Day22.kt
@@ -1,8 +1,11 @@
 package fr.pasithee.aoc2019
 
+import java.lang.IllegalArgumentException
+import java.math.BigInteger
+
 class Day22 {}
 
-class CardShuffler(lastCard : Long) {
+class CardShuffler(lastCard : Int) {
 
     var deck = (0..lastCard).toList()
 
@@ -19,7 +22,7 @@ class CardShuffler(lastCard : Long) {
     }
 
     fun dealWithIncrement(n : Int) {
-        val tmpDeck = LongArray(deck.size)
+        val tmpDeck = IntArray(deck.size)
         var pos = 0
         for (card in deck) {
             tmpDeck[pos] = card
@@ -43,10 +46,137 @@ class CardShuffler(lastCard : Long) {
     }
 }
 
+class LargeCardShuffler(val lastCard : Long) {
+    fun cardCutN(n : Long, pos : Long) : Long {
+        if (n > 0) {
+           return (n + pos) % (lastCard + 1)
+        } else {
+            return cardCutN(lastCard + 1 + n, pos)
+        }
+    }
+
+    fun cardDealWithIncrement(n: Long, pos : Long) : Long {
+        if (pos == 0L) {
+            return 0
+        }
+        val modInv = modInverse(n, lastCard + 1)
+        val res = (BigInteger.valueOf(pos) * BigInteger.valueOf(modInv)) % (BigInteger.valueOf(lastCard + 1))
+        return res.toLong()
+    }
+
+    fun cardDealStack(pos : Long) : Long {
+        return lastCard - pos
+    }
+
+    fun shuffle(ops: List<String>, pos: Long) : Long {
+        if (ops.isEmpty()) {
+            return pos
+        }
+        val op = ops.last()
+        val prevPos = doOperation(op, pos)
+        return shuffle(ops.dropLast(1), prevPos)
+    }
+
+    fun doOperation(op : String, pos : Long) : Long {
+        return when {
+            op.startsWith("cut ") -> cardCutN(op.replace("cut ", "").toLong(), pos)
+            op.startsWith("deal with increment ") -> cardDealWithIncrement((op.replace("deal with increment ", "").toLong()), pos)
+            op.startsWith("deal into new stack") -> cardDealStack(pos)
+            else -> throw(IllegalArgumentException("invalid operation"))
+        }
+    }
+
+}
+
+fun modInverse(aInit: Long, mInit: Long): Long {
+    return BigInteger.valueOf(aInit).modPow(BigInteger.valueOf(-1), BigInteger.valueOf(mInit)).toLong()
+}
+
+class ReducedCardShuffler(val numCards : BigInteger, val ops: List<String>) {
+    val a: BigInteger
+    val b: BigInteger
+
+    init {
+        var tmpA = BigInteger.valueOf(1)
+        var tmpB = BigInteger.valueOf(0)
+
+        for (op in ops.reversed()) {
+            val res = when {
+                op.startsWith("cut ") -> cardCutN(op.replace("cut ", "").toLong(), tmpA, tmpB)
+                op.startsWith("deal with increment ") -> cardDealWithIncrement(
+                    (op.replace(
+                        "deal with increment ",
+                        ""
+                    ).toLong()), tmpA, tmpB
+                )
+                op.startsWith("deal into new stack") -> cardDealStack(tmpA, tmpB)
+                else -> throw(IllegalArgumentException("invalid operation"))
+            }
+            tmpA = res.first
+            tmpB = res.second
+        }
+
+        a = tmpA
+        b = tmpB
+    }
+
+    fun transform(a: BigInteger, b: BigInteger, a2: BigInteger, b2: BigInteger): Pair<BigInteger, BigInteger> {
+        return Pair((a * a2) % numCards, (a2 * b + b2) % numCards)
+    }
+
+    fun cardCutN(n: Long, a: BigInteger, b: BigInteger): Pair<BigInteger, BigInteger> =
+        transform(a, b, BigInteger.valueOf(1), BigInteger.valueOf(n))
+
+
+    fun cardDealWithIncrement(n: Long, a: BigInteger, b: BigInteger): Pair<BigInteger, BigInteger> {
+        val modInv = modInverse(n, numCards.toLong())
+        return transform(a, b, BigInteger.valueOf(modInv), BigInteger.valueOf(0))
+    }
+
+    fun cardDealStack(a : BigInteger, b : BigInteger) = transform(a, b, BigInteger.valueOf(-1), BigInteger.valueOf(-1))
+
+    fun apply(pos : BigInteger) : Long {
+        val v = ((pos * a + b) % numCards)
+        return if (v < BigInteger.ZERO) { (v + numCards).toLong() } else { v.toLong() }
+    }
+
+    fun apply(pos : BigInteger, numOps : BigInteger) : Long {
+        val powA = a.modPow(numOps, numCards)
+        val firstTerm = (powA * (pos % numCards)) % numCards
+
+       /* var v = pos
+        for (i in 1..numOps.toLong()) {
+            v = BigInteger.valueOf(apply(v))
+        }
+        return v.toLong()*/
+        val inverse1MinusA = (BigInteger.valueOf(1) - a).modPow(BigInteger.valueOf(-1), numCards)
+
+        val geomSeq = ((BigInteger.valueOf(1) - powA)%numCards * inverse1MinusA%numCards)%numCards
+
+        val secondTerm = (b % numCards) * (geomSeq % numCards) % numCards
+        val sum = ((firstTerm + secondTerm) % numCards)
+        return if (sum < BigInteger.ZERO) { (sum + numCards).toLong()} else {sum.toLong()}
+    }
+}
+
 fun main() {
     val ops = readFileToStrings(Day22().javaClass.getResource("day22.txt").path)
     val deck = CardShuffler(10006)
     deck.shuffle(ops)
     println(deck.deck.indexOf(2019))
+
+    val deckInLargeShuffler = LargeCardShuffler(10006)
+    println(deckInLargeShuffler.shuffle(ops, 6431))
+
+    val reducedCardShuffler = ReducedCardShuffler(BigInteger.valueOf(10007), ops)
+    println(reducedCardShuffler.apply(BigInteger.valueOf(6431)))
+    println(reducedCardShuffler.apply(BigInteger.valueOf(2019)))
+    println(reducedCardShuffler.apply(BigInteger.valueOf(8420)))
+    println(reducedCardShuffler.apply(BigInteger.valueOf(6431), BigInteger.valueOf(3)))
+
+    val part2Shuffler = ReducedCardShuffler(BigInteger.valueOf(119315717514047), ops)
+    println(part2Shuffler.apply(BigInteger.valueOf(2020), BigInteger.valueOf(101741582076661)))
+
 }
+
 
--- src/test/kotlin/fr/pasithee/aoc2019/Day22Test.kt
+++ src/test/kotlin/fr/pasithee/aoc2019/Day22Test.kt
@@ -2,6 +2,7 @@ package fr.pasithee.aoc2019
 
 import org.junit.Assert.*
 import org.junit.Test
+import java.math.BigInteger
 
 class Day22Test {
     @Test
@@ -49,5 +50,77 @@ class Day22Test {
         val deck = CardShuffler(9)
         deck.shuffle(ops)
         assertEquals(listOf(9,2,5,8,1,4,7,0,3,6), deck.deck)
+    }
+
+    @Test
+    fun largeDeckStack() {
+        val deck = LargeCardShuffler(9)
+        assertEquals(6, deck.cardDealStack(3))
+    }
+
+    @Test
+    fun largeDeckCut() {
+        val deck = LargeCardShuffler(9)
+        assertEquals(0, deck.cardCutN(3, 7))
+        assertEquals(8, deck.cardCutN(3, 5))
+        assertEquals(8, deck.cardCutN(-4, 2))
+        assertEquals(3, deck.cardCutN(-4, 7))
+    }
+
+    @Test
+    fun largeDeckDeal() {
+        val deck = LargeCardShuffler(9)
+        assertEquals(7, deck.cardDealWithIncrement(3, 1))
+        assertEquals(0, deck.cardDealWithIncrement(5, 0))
+        assertEquals(1, deck.cardDealWithIncrement(3, 3))
+        assertEquals(6, deck.cardDealWithIncrement(3, 8))
+        assertEquals(8, deck.cardDealWithIncrement(3, 4))
+    }
+
+    @Test
+    fun largeDeckOperations() {
+        val ops = listOf(
+            "deal into new stack",
+            "cut -2",
+            "deal with increment 7",
+            "cut 8",
+            "cut -4",
+            "deal with increment 7",
+            "cut 3",
+            "deal with increment 9",
+            "deal with increment 3",
+            "cut -1"
+        )
+        val deck = LargeCardShuffler(9)
+
+        assertEquals(9, deck.shuffle(ops, 0))
+        assertEquals(2, deck.shuffle(ops, 1))
+        assertEquals(5, deck.shuffle(ops, 2))
+        assertEquals(8, deck.shuffle(ops, 3))
+        assertEquals(1, deck.shuffle(ops, 4))
+        assertEquals(4, deck.shuffle(ops, 5))
+        assertEquals(7, deck.shuffle(ops, 6))
+        assertEquals(0, deck.shuffle(ops, 7))
+        assertEquals(3, deck.shuffle(ops, 8))
+        assertEquals(6, deck.shuffle(ops, 9))
+    }
+
+    @Test
+    fun reducedDeckStack() {
+        val ops = listOf("deal into new stack")
+        val deck = ReducedCardShuffler(BigInteger.valueOf(10), ops)
+        assertEquals(6, deck.apply(BigInteger.valueOf(3)))
+    }
+
+    @Test
+    fun reducedDeckDealPositive() {
+        val ops = listOf("deal with increment 3")
+        val deck = ReducedCardShuffler(BigInteger.valueOf(10), ops)
+        assertEquals(7, deck.apply(BigInteger.ONE))
+    }
+
+    @Test
+    fun testInverseModulo() {
+        assertEquals(4, modInverse(3, 11))
     }
 }
